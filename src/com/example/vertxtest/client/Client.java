26c2e5be7ef7d6276281017ef8da136b1cedb4ac
Pradyumna Achar K <prkadalb@cisco.com>
2017-02-27 17:57:14 +0530

    Updated with timeout set

--- src/com/example/vertxtest/client/Client.java
+++ src/com/example/vertxtest/client/Client.java
@@ -8,6 +8,7 @@ import com.example.vertxtest.server.Server;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
 
 public class Client {
 	private static final Logger log = LogManager.getLogger(Client.class);
@@ -18,44 +19,57 @@ public class Client {
 		httpClientOptions.setMaxWaitQueueSize(3);
 		HttpClient httpClient = clientVertx.createHttpClient(httpClientOptions);
 		
-		httpClient.get(Server.PORT, "localhost", "/").setTimeout(3000).exceptionHandler(t->{
+		//This first request will block the pool for 15 seconds
+		final HttpClientRequest req1 = httpClient.get(Server.PORT, "localhost", "/").setTimeout(15000).exceptionHandler(t->{
 			log.info("Request 1 saw exception {}", t.getMessage());
-		}).handler(resp->{
+		});
+		req1.handler(resp->{
 			log.info("Request 1 request recieved response.");
-		}).end();
+			req1.setTimeout(15000);
+		});
+		req1.end();
+		
 		
 		try {
-			Thread.sleep(500); //give some time to let the first request start
+			Thread.sleep(1000); //give some time to let the first request start
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		for(int i=0; i<3; i++){ //these 3 requests will initially fill up the wait queue. 1 should be sent after 3 seconds (when the first request above ends via timeout), and 2 should remain in the wait queue.
+		
+		for(int i=0; i<3; i++){ //these 3 requests will fill up the wait queue for 5 seconds.
 			final int reqNum = i+2;
+			final HttpClientRequest req2 =
 			httpClient.get(Server.PORT, "localhost", "/").setTimeout(5000).exceptionHandler(t->{
 				log.info("Request {} saw exception {}", reqNum, t.getMessage());
-			}).handler(resp->{
-				log.info("Request {} recieved response.");
+			});
+			req2.handler(resp->{
+				log.info("Request {} recieved response.", reqNum);
+				req2.setTimeout(5000);
 			}).end();
 		}
-		//After 3+5 = 7 seconds, the wait queue size should reduce to 2.
+		//1+5 = 6 seconds, all three waiting requests should have timed out.
 		try {
-			Thread.sleep(10000); //wait for 10 seconds, to allow for some leniency. The wait queue should have at least one slot open by now.
+			Thread.sleep(10000); //wait for 10 seconds, to allow for some leniency. The wait queue should have timed out by now.
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		log.info("by this time, you should have seen at one timeout exception in the logs above, thereby freeing up one slot in the wait queue");
-		log.info("this new request should not get a connection-pool-busy exception");
-		httpClient.get(Server.PORT, "localhost", "/").setTimeout(10000).exceptionHandler(t->{
+		log.info("by this time, you should have seen three timeout exceptions in the logs above, thereby freeing up all three slots in the wait queue");
+
+		log.info("Sending a new request. This should not get a connection-wait-queue-size-maxed out exception");
+		HttpClientRequest req5 = httpClient.get(Server.PORT, "localhost", "/").setTimeout(10000).exceptionHandler(t->{
 			log.info("Request 5 saw exception {}", t.getMessage());
-		}).handler(resp->{
+		});
+		req5.handler(resp->{
 			log.info("Request 5 recieved response. HTTP ", resp.statusCode());
+			req5.setTimeout(1000);
 		}).end();
 		try {
-			Thread.sleep(30000); //wait for 30 seconds, and check again
+			Thread.sleep(30000); //wait for 30 seconds, and check again. By this time, the connection pool itself should have got freed up.
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		log.info("by this time, the connection pool should be free. This request should go through");
 		httpClient.get(Server.PORT, "localhost", "/").setTimeout(10000).exceptionHandler(t->{
 			log.info("Request 6 saw exception {}", t.getMessage());
 		}).handler(resp->{
